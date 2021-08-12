70e89907586e2b3e24a6d90c989b32652b2b4e43
Parth-Coditas <parth.patel@coditas.com>
2021-08-12 13:05:37 +0530

    Added workaround for fetch error

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,10 @@
-async function searchMealByName_function(searchQuery_string){
-    await fetch(`//themealdb.com/api/json/v1/1/search.php?s=${searchQuery_string}`,{mode: 'no-cors'}).then((response) => {
-        return response.text();
-    // if(response.status === 200){
-    //         return response.json();
-    //     } else {
-    //         console.log(response);
-    //         console.log(`Response Status: ${response.status}`);
-    //     }
-    // }).then((response) => {console.log(response);});
-}).then(response => {console.log(response);})}
+async function searchMealByName_function(searchQuery_string) {
+  try {
+    const response = await fetch(
+      `https://themealdb.com/api/json/v1/1/search.php?s=${searchQuery_string}`,
+    );
+    console.log("response", response);
+  } catch (error) {
+    console.log("error", error.message);
+  }
+}
