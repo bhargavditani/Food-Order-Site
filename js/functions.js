723e44e433d1c304c0e60ade070739ccc11bd3d7
Bhargav Ditani <bhargavditani@gmail.com>
2021-08-13 11:35:03 +0530

    Integrated Search API

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,23 @@
 async function searchMealByName_function(searchQuery_string) {
-  try {
-    const response = await fetch(
-      `https://themealdb.com/api/json/v1/1/search.php?s=${searchQuery_string}`,
-    );
-    console.log("response", response);
-  } catch (error) {
-    console.log("error", error.message);
+    try {
+       let response = await fetch(`https://themealdb.com/api/json/v1/1/search.php?s=${searchQuery_string}`);
+        if (response.status === 200) {
+          return await response.json();
+        } else {
+          throw new Error('Something Went Wrong');
+        }
+  } catch(error) {
+      throw error;
+  }//catch closed
+}
+
+async function mealByNameData_function(response){
+  if (response.meals !== null){
+    sessionStorage.setItem('response', JSON.stringify(response));
+    const inputField = document.querySelector('#search');
+    inputField.value='';
+    location.assign('../search-result.html');
+  } else {
+    throw new Error('Nothing Found!')
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-document.querySelector('.hamburger-menu').addEventListener('click',(event)=>{
+document.querySelector('.hamburger-menu').addEventListener('click', (event) => {
     const hamburgerMenuItems_El = document.querySelector('.hamburger-menu-items');
-    if (hamburgerMenuItems_El.style.display === 'none'){
+    if (hamburgerMenuItems_El.style.display === 'none') {
         hamburgerMenuItems_El.style.display = 'inline-flex';
     } else {
         hamburgerMenuItems_El.style.display = 'none';
     }
 });
 
-document.querySelector('#search-button').addEventListener('click',(event) => {
+document.querySelector('#search-button').addEventListener('click', (event) => {
     const searchInput_El = document.querySelector('#search');
-    let searchQuery_string = searchInput_El.value.trim().toLowerCase();
-    if(searchQuery_string.length > 0 && searchQuery_string.length < 255){
-        searchMealByName_function(searchQuery_string);
-    }
+    const searchQuery_string = searchInput_El.value.trim().toLowerCase();
+    try {
+        if (searchQuery_string.length > 0 && searchQuery_string.length < 255) {
+            searchMealByName_function(searchQuery_string).then(response => mealByNameData_function(response)).catch(error => {
+                alert(error.message);
+            });
+        } else {
+            throw new Error('Enter Valid Data');
+        }
+    } catch (error) {
+        alert(error.message);
+    }//catch block closed
 });
-
-// document.querySelector('#search').addEventListener('change', (event) => {
-//     let searchQuery_string = event.target.value.trim().toLowerCase();
-//     searchMealByName_function(searchQuery_string);
-// })
