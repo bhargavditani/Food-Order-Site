0403c7af5d5c582020b2bd200477335fef4896da
Bhargav Ditani <bhargavditani@gmail.com>
2021-08-13 16:37:37 +0530

    List/Filter by Category added

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,53 @@
-async function searchMealByName_function(searchQuery_string) {
-    try {
-       let response = await fetch(`https://themealdb.com/api/json/v1/1/search.php?s=${searchQuery_string}`);
+async function searchMealByName_function(searchQuery_string, categoryValue) {
+  try {
+    if (searchQuery_string.length !== 0 && categoryValue === 'All') {
+      const response = await fetch(`https://themealdb.com/api/json/v1/1/search.php?s=${searchQuery_string}`);
+      if (response.status === 200) {
+        return await response.json();
+      } else {
+        throw new Error('Something Went Wrong');
+      }
+    } else if (searchQuery_string.length !== 0 && categoryValue !== 'All') {
+        const response = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${categoryValue}`);
         if (response.status === 200) {
           return await response.json();
         } else {
           throw new Error('Something Went Wrong');
         }
-  } catch(error) {
-      throw error;
+    } else if (searchQuery_string.length === 0 && categoryValue !== 'All') {
+        const response = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${categoryValue}`,);
+        if (response.status === 200) {
+          return await response.json();
+        } else {
+          throw new Error('Something Went Wrong');
+        }
+    } else {
+        throw new Error('Enter Valid Data');
+    }
+  } catch (error) {
+    throw error;
   }//catch closed
 }
 
-async function mealByNameData_function(response){
-  if (response.meals !== null){
-    sessionStorage.setItem('response', JSON.stringify(response));
-    const inputField = document.querySelector('#search');
-    inputField.value='';
-    location.assign('../search-result.html');
+async function mealByNameData_function(response, searchQuery_string, categoryValue) {
+  let result;
+  const inputField = document.querySelector('#search');
+  if (response.meals !== null) {
+    if (categoryValue === 'All') {
+      sessionStorage.setItem('response', JSON.stringify(response));
+      inputField.value = '';
+      location.assign('../search-result.html');
+    } else {
+      result = response.meals.filter(meal => meal.strMeal.toLowerCase().includes(searchQuery_string));
+      if (result.length) {
+        sessionStorage.setItem('response', JSON.stringify({meals: result}));
+        inputField.value = '';
+        location.assign('../search-result.html');
+      }
+      else {
+        throw new Error('Nothing Found!')
+      }
+    }
   } else {
     throw new Error('Nothing Found!')
   }
--- js/index.js
+++ js/index.js
@@ -1,24 +1,46 @@
 document.querySelector('.hamburger-menu').addEventListener('click', (event) => {
     const hamburgerMenuItems_El = document.querySelector('.hamburger-menu-items');
     if (hamburgerMenuItems_El.style.display === 'none') {
-        hamburgerMenuItems_El.style.display = 'inline-flex';
+        hamburgerMenuItems_El.style.display = 'flex';
     } else {
         hamburgerMenuItems_El.style.display = 'none';
     }
 });
 
+
 document.querySelector('#search-button').addEventListener('click', (event) => {
     const searchInput_El = document.querySelector('#search');
+    const category_El = document.querySelector('#category');
     const searchQuery_string = searchInput_El.value.trim().toLowerCase();
     try {
-        if (searchQuery_string.length > 0 && searchQuery_string.length < 255) {
-            searchMealByName_function(searchQuery_string).then(response => mealByNameData_function(response)).catch(error => {
+        if (searchQuery_string.length >= 0 && searchQuery_string.length < 255) {
+            searchMealByName_function(searchQuery_string, category_El.value).then(response => mealByNameData_function(response, searchQuery_string, category_El.value)).catch(error => {
                 alert(error.message);
             });
-        } else {
+        } 
+        else {
             throw new Error('Enter Valid Data');
         }
     } catch (error) {
         alert(error.message);
     }//catch block closed
 });
+
+function populateCategoryDropDown_function(){
+    // debugger
+    const select_El = document.querySelector('#category');
+    fetch('https://themealdb.com/api/json/v1/1/list.php?c=list',).then(response => {
+        if (response.status === 200) {
+            return response.json();
+        }
+    }).then(response => {
+        response.meals.forEach(category => {
+            const options_El = document.createElement('option');
+            options_El.innerHTML = category.strCategory;
+            options_El.setAttribute('value',category.strCategory);
+            select_El.appendChild(options_El);
+        });
+    });
+}
+
+populateCategoryDropDown_function();
